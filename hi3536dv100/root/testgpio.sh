9455eeb26a08e370cc24023ae2c9aba1172ac1e4
whoim2 <whoim@mail.ru>
2023-03-23 13:32:52 +0300

    Update testgpio.sh

--- hi3536dv100/root/testgpio.sh
+++ hi3536dv100/root/testgpio.sh
@@ -22,11 +22,11 @@ function get_gpio {
   cat /sys/class/gpio/gpio${1}/value
 }
 
-for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47
-do
-  gpio_setup $i in
-  echo $i: `get_gpio $i`
-done
+#for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47
+#do
+#  gpio_setup $i in
+#  echo $i: `get_gpio $i`
+#done
 
 gpio_setup 10 out
 set_gpio 10 1
