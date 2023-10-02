675825c250269049531b386bbe3aaea2ce4dbed7
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2023-10-02 11:04:26 +0100

    Update channels.sh

--- gk7205v200/root/channels.sh
+++ gk7205v200/root/channels.sh
@@ -2,13 +2,17 @@ echo $1 $2 >>/tmp/channels.log
 
 #channel 5
 if [ $1 -eq 5 ]; then
-    if [ $2 -lt 1600 ]; then
-      /root/1080.sh
-    else
-      /root/720.sh
+    if [ $2 -gt 1600 ]; then
+      CURRENT_SIZE=`yaml-cli -g .video0.size`
+      if [ $CURRENT_SIZE == '1280x720' ]; then
+        /root/1080.sh
+        sleep 3
+      else
+        /root/720.sh
+        sleep 3
+      fi
     fi
 fi
-
 
 #channel 6
 if [ $1 -eq 6 ]; then
@@ -32,4 +36,22 @@ if [ $1 -eq 7 ]; then
       killall -1 majestic
     fi
 fi
+
+#channel 8
+if [ $1 -eq 8 ]; then
+    if [ $2 -gt 1600 ]; then
+      CURRENT_BITRATE=`yaml-cli -g .video0.bitrate`
+      if [ $CURRENT_BITRATE -lt 7168 ]; then
+        NEW_BITRATE="$(($CURRENT_BITRATE+1024))"
+      else
+        NEW_BITRATE="1024"
+        sleep 2
+      fi
+      yaml-cli -s .video0.bitrate $NEW_BITRATE
+      sleep .2
+      /root/kill.sh
+    fi
+    sleep 3
+fi
+
 exit 1
