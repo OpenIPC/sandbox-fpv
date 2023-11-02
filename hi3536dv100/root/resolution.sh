022b5cea7b9a48cbbb8f00c6ed579e4755dd97e0
Signor Pellegrino <68112357+FlyRouter@users.noreply.github.com>
2023-11-02 21:19:42 +0300

    Update resolution.sh

--- hi3536dv100/root/resolution.sh
+++ hi3536dv100/root/resolution.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 ARG="`cat /etc/vdec.conf`"
 value=${ARG#*mode=}
 value2=${value%%p60*}
@@ -6,4 +8,5 @@ if [ $value2 == '720' ]; then
 elif [ $value2 == '1080' ]; then
     sed -i -e 's/mode=1080p60/mode=720p60/g' /etc/vdec.conf
 fi
-reboot
+reboot  # Please probe this for re-read config: killall -1 vdec
+
