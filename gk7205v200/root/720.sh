95e94d4850994686af04029865e1368500bc9941
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2023-12-31 12:31:06 +0000

    Update 720.sh

--- gk7205v200/root/720.sh
+++ gk7205v200/root/720.sh
@@ -3,6 +3,7 @@
 yaml-cli -s .video0.size 1280x720
 yaml-cli -s .video0.fps 30
 yaml-cli -s .isp.sensorConfig /etc/sensors/imx415.bin
+yaml-cli -s .isp.exposure 10
 yaml-cli -s .video0.crop 0x0x1280x720
 sleep .2
 /root/kill.sh
