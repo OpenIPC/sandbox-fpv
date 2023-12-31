87a39bf8fbee4e9f2f8fb50c76cebb72e2723a46
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2023-12-31 12:23:11 +0000

    Add  resolutions switch for Sigmastar SSC338Q-IMX415
    Add  resolutions switch for Sigmastar SSC338Q-IMX415

--- gk7205v200/root/channels.sh
+++ gk7205v200/root/channels.sh
@@ -15,11 +15,46 @@ if [ $1 -eq 5 ]; then
 fi
 
 #channel 6
+#if [ $1 -eq 6 ]; then
+#    if [ $2 -lt 1600 ]; then
+#      /root/ircut.sh off
+#    else
+#      /root/ircut.sh on
+#    fi
+#fi
+
 if [ $1 -eq 6 ]; then
-    if [ $2 -lt 1600 ]; then
-      /root/ircut.sh off
-    else
-      /root/ircut.sh on
+    if [ $2 -gt 1600 ]; then
+      CURRENT_SIZE=`yaml-cli -g .video0.size`
+      CURRENT_FPS=`yaml-cli -g .video0.fps`
+      if [ $CURRENT_SIZE == '1280x720' ]  && [ $CURRENT_FPS == '30' ] ; then
+        /root/720b.sh
+        sleep 3
+      elif [ $CURRENT_SIZE == '1280x720' ]  && [ $CURRENT_FPS == '60' ] ; then
+        /root/720c.sh
+        sleep 3
+      elif [ $CURRENT_SIZE == '1280x720' ]  && [ $CURRENT_FPS == '90' ] ; then
+        /root/720d.sh
+        sleep 3
+      elif [ $CURRENT_SIZE == '1280x720' ]  && [ $CURRENT_FPS == '120' ] ; then
+        /root/1080.sh
+        sleep 3
+      elif [ $CURRENT_SIZE == '1920x1080' ] && [ $CURRENT_FPS == '30' ] ; then
+        /root/1080b.sh
+        sleep 3
+      elif [ $CURRENT_SIZE == '1920x1080' ] && [ $CURRENT_FPS == '60' ] ; then
+        /root/1080c.sh
+        sleep 3
+      elif [ $CURRENT_SIZE == '1920x1080' ] && [ $CURRENT_FPS == '90' ] ; then
+        /root/3K.sh
+        sleep 3
+      elif [ $CURRENT_SIZE == '3200x1800' ]; then
+        /root/4K.sh
+        sleep 3
+      else
+        /root/720.sh
+        sleep 3
+      fi
     fi
 fi
 
