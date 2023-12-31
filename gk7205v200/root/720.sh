197ead363ccc83a2f8ab523ebe2f0ca01beab2ad
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2023-12-31 12:29:28 +0000

    Update 720.sh

--- gk7205v200/root/720.sh
+++ gk7205v200/root/720.sh
@@ -1,9 +1,8 @@
-yaml-cli -s .isp.sensorConfig /etc/sensors/720p30_imx307_50.ini
-#yaml-cli -s .video0.bitrate 12288
-#yaml-cli -s .video0.bitrate 7168
-yaml-cli -s .video0.bitrate 3072
+#yaml-cli -s .isp.sensorConfig /etc/sensors/720p30_imx307_50.ini #use this for imx307
+#yaml-cli -s .video0.fps 50 #use thise for imx307
 yaml-cli -s .video0.size 1280x720
-yaml-cli -s .video0.fps 50
+yaml-cli -s .video0.fps 30
+yaml-cli -s .isp.sensorConfig /etc/sensors/imx415.bin
 yaml-cli -s .video0.crop 0x0x1280x720
 sleep .2
 /root/kill.sh
