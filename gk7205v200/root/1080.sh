e97cd121ad6155d5d9fe7651143adb24c60946e8
MarioFPV <57532232+MarioFPVdev@users.noreply.github.com>
2023-12-31 12:30:42 +0000

    Update 1080.sh

--- gk7205v200/root/1080.sh
+++ gk7205v200/root/1080.sh
@@ -1,7 +1,6 @@
-yaml-cli -s .isp.sensorConfig /etc/sensors/imx307_i2c_2l_1080p.ini
-#yaml-cli -s .video0.bitrate 12288
-#yaml-cli -s .video0.bitrate 7168
-yaml-cli -s .video0.bitrate 3072
+#yaml-cli -s .isp.sensorConfig /etc/sensors/imx307_i2c_2l_1080p.ini #use this for imx307
+yaml-cli -s .isp.sensorConfig /etc/sensors/imx415.bin
+yaml-cli -s .isp.exposure 10
 yaml-cli -s .video0.size 1920x1080
 yaml-cli -s .video0.fps 30
 yaml-cli -s .video0.crop 0x0x1920x1080
